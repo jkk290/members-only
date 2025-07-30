df4c790f917bb815f19f026c84153b401905aeef
jkk290 <158115736+jkk290@users.noreply.github.com>
2025-07-30 13:49:35 -1000

    Implement validation for sign up form

--- controllers/appController.js
+++ controllers/appController.js
@@ -1,6 +1,27 @@
 const passport = require('passport');
 const db = require('../storages/queries');
 const bcrypt = require('bcryptjs');
+const { body, validationResult } = require('express-validator');
+
+const usernameErr = 'must be an email format, example@domain.com.';
+const passwordLengthErr = 'must by at least 8 characters long.';
+const passwordContainsErr = 'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character (!@#$%^&*)';
+const passwordConfirmErr = 'Passwords do not match.';
+
+const validateUser = [
+    body('username').trim()
+    .isEmail().withMessage(`Email ${usernameErr}`),
+    body('password').trim()
+    .isLength({ min: 8 }).withMessage(`Password ${passwordLengthErr}`)
+    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/).withMessage(passwordContainsErr),
+    body('passwordConfirm').trim()
+    .custom((value, { req }) => {
+        if (value !== req.body.password) {
+            throw new Error(passwordConfirmErr);
+        }
+        return true;
+    }).withMessage(passwordConfirmErr)
+];
 
 exports.loginGet = (req, res) => {
     res.render('login', {
@@ -14,21 +35,49 @@ exports.signupGet = (req, res) => {
     });
 };
 
-exports.signupPost = async (req, res, next) => {
-    try {
+// exports.signupPost = async (req, res, next) => {
+//     try {
+//         const hashedPassword = await bcrypt.hash(req.body.password, 10);
+//         const user = {
+//             firstName: req.body.firstName,
+//             lastName: req.body.lastName,
+//             username: req.body.username,
+//             password: hashedPassword
+//         };
+//         db.addUser(user);
+//         res.redirect('/');
+//     } catch (error) {
+//         return next(error);
+//     }
+// };
+
+exports.signupPost = [
+    validateUser, 
+    async (req, res) => {
+        const errors = validationResult(req);
+        
+        if (!errors.isEmpty()) {
+            return res.status(400).render('signup', {
+                title: 'Account Sign Up',
+                errors: errors.array(),
+            });
+        }
+
+        try {
         const hashedPassword = await bcrypt.hash(req.body.password, 10);
         const user = {
             firstName: req.body.firstName,
             lastName: req.body.lastName,
             username: req.body.username,
             password: hashedPassword
-        };
-        db.addUser(user);
-        res.redirect('/');
-    } catch (error) {
-        return next(error);
+            };
+            db.addUser(user);
+            res.redirect('/');
+        } catch (error) {
+            return next(error);
+        }; 
     }
-};
+];
 
 exports.loginPost = passport.authenticate('local', {
     successRedirect: '/',
